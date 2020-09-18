b3bfce1dbe429d7f723cc154789d0c401219a86c
mwr2019 <mwr2019@users.noreply.github.com>
2020-09-18 18:43:30 +0200

    profil/kategori

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,19 +1,18 @@
 package com.example.myapplication
 
-import RecyclerView.RecyclerView.EventRecyclerAdapter
 import android.os.Bundle
-import android.widget.Toolbar
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.example.myapplication.event.Event_liste_fragment
 import com.example.myapplication.fragments.hjem.HjemFragment
+import com.example.myapplication.fragments.kategori.KategoriFragment
 import com.example.myapplication.fragments.mineevents.MineEventFragment
 import com.example.myapplication.fragments.nyttevent.NyttEventFragment
-import com.example.myapplication.fragments.kategori.KategoriFragment
 import com.example.myapplication.fragments.profil.ProfilFragment
 import com.example.myapplication.fragments.venner.VennerFragment
 import kotlinx.android.synthetic.main.activity_main.*
-
+import kotlinx.android.synthetic.main.fragment_profil.*
 
 
 class MainActivity : AppCompatActivity() {
@@ -22,7 +21,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val kategoriFragment = KategoriFragment()
+
+        //redigerKnapp.setOnClickListener {view?.findNavController()?.navigate(R.id.action_event_liste_fragment_to_eventFragment)  }
+
+        //val kategoriFragment = KategoriFragment()
         val eventListeFragment = Event_liste_fragment()
         val hjemFragment = HjemFragment()
         val mineEventFragment = MineEventFragment()
@@ -34,7 +36,7 @@ class MainActivity : AppCompatActivity() {
 
         toolbar.setOnMenuItemClickListener() {
             when(it.itemId) {
-                R.id.kategori -> makeCurrentFragment(kategoriFragment)
+               // R.id.kategori -> makeCurrentFragment(kategoriFragment)
             }
             true
         }
@@ -50,7 +52,6 @@ class MainActivity : AppCompatActivity() {
             true
         }
     }
-
     private fun makeCurrentFragment(fragment: Fragment) =
         supportFragmentManager.beginTransaction().apply {
             replace(R.id.container, fragment)
--- app/src/main/java/com/example/myapplication/fragments/kategori/KategoriFragment.kt
+++ app/src/main/java/com/example/myapplication/fragments/kategori/KategoriFragment.kt
@@ -1,23 +1,45 @@
 package com.example.myapplication.fragments.kategori
 
+import android.app.Dialog
+import android.content.DialogInterface
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AlertDialog
+import androidx.fragment.app.DialogFragment
 import com.example.myapplication.R
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import kotlinx.android.synthetic.main.fragment_kategori.*
 
 /**
  * A simple [Fragment] subclass.
  */
-class KategoriFragment : Fragment() {
-
+class KategoriFragment(private val callbackListener: CallbackListener) : DialogFragment() {
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
+        isCancelable = false
         return inflater.inflate(R.layout.fragment_kategori, container, false)
+    }
+    override fun getTheme(): Int {
+        return R.style.DialogTheme
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        button.setOnClickListener {
+            //send back data to PARENT fragment using callback
+            callbackListener.onDataReceived(editText.text.toString())
+            // Now dismiss the fragment
+            dismiss()
+        }
+
+    }
+    interface CallbackListener {
+        fun onDataReceived(data: String)
     }
 
 }
