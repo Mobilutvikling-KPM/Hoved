23bf1d835caf22b8db1cf0f8a84969758df58579
Patrick Lorentzen <patrick.lorentzen@outlook.com>
2020-10-05 18:37:12 +0200

    Lagt inn modelview og livedata på event

--- app/src/main/java/com/example/myapplication/viewmodels/EventViewModel.kt
+++ app/src/main/java/com/example/myapplication/viewmodels/EventViewModel.kt
@@ -6,17 +6,37 @@ import androidx.lifecycle.LiveData
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.loader.content.AsyncTaskLoader
 
-class EventViewModel: ViewModel() {
+class EventViewModel(type: Int): ViewModel() {
 
     private var mEvents: MutableLiveData<List<Event>>
     private var eventRepo: EventRepository= EventRepository().getTheInstance()
+    private var mIsUpdating: MutableLiveData<Boolean> = MutableLiveData()
+    var type: Int = type
 
     init {
-        mEvents = eventRepo.getEvents()  //Henter data fra databasen. EVent Repository
+        mEvents = eventRepo.getEvents(type)  //Henter data fra databasen. EVent Repository
     }
 
+    //Skal repsentere om data er hentet eller ikke
+
+    fun leggTilEvent(event: Event){
+        mIsUpdating.value = true;
+
+       var liste: ArrayList<Event> = mEvents.value as ArrayList<Event>
+        if (liste != null) {
+            liste.add(event)
+            mEvents.postValue(liste)
+            mIsUpdating.postValue(false)
+        }
+
+    }
     fun getEvents(): LiveData<List<Event>>{
         return mEvents
+    }
+
+    fun getIsUpdating(): LiveData<Boolean>{
+        return mIsUpdating
     }
 }
