346d6f35e6b0ced83f69a42d14c773fab24d9407
mwr2019 <mwr2019@users.noreply.github.com>
2020-10-06 12:11:07 +0200

    login screen +

--- app/src/main/java/com/example/myapplication/fragments/profil/RedigerProfilFragment.kt
+++ app/src/main/java/com/example/myapplication/fragments/profil/RedigerProfilFragment.kt
@@ -1,10 +1,15 @@
 package com.example.myapplication.fragments.profil
 
+import android.app.AlertDialog
+import android.content.Context
+import android.content.DialogInterface
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.afollestad.materialdialogs.MaterialDialog
+import com.afollestad.materialdialogs.customview.customView
 import com.example.myapplication.R
 
 
@@ -22,5 +27,6 @@ class RedigerProfilFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_rediger_profil, container, false)
+
     }
 }
